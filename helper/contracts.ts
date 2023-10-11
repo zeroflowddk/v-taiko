c1e6c02821f20906825adc40eaf9231d9cf09ad3
Oʀɪɢᴀᴍɪ <aoyamalinux@gmail.com>
2023-10-11 20:57:04 +0300

    contracts address

---
+++ helper/contracts.ts
@@ -0,0 +1,11 @@
+import { Hex } from "viem";
+
+export const taikoContracts = {
+    router: '0x5293Bb897db0B64FFd11E0194984E8c5F1f06178'as Hex,
+    swap: '0xCC287c5F7455Aa091C34CA7C73228863a99B0070' as Hex,
+    mint: '0xB80CDCCC10aa91CE60FB7F758e48Fa8e057FA57b' as Hex,
+    fabric: '0xF69992828179C1D4A13E048c1B6692fB656372fA' as Hex, 
+    eth : '0x7be7937A395FD3B527F428124f6a33E10D1C0638'  as Hex,
+    horse: '0x9833DcA11f178dbaF2b88da42557DA2970534430' as Hex,
+    ttkjo: '0xe705498492D0aE94CA9365D395D2C6924F24F445' as Hex
+};
